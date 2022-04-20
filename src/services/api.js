29de12ab13289bd2166c4ce051899e9b9d23cd59
DESKTOP-L4IT8DB\Antonio <limaantoniocarlos20@gmail.com>
2022-04-19 22:40:33 -0300

    api client

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 const api = axios.create({
-  baseURL: 'http://https://api-mylms.herokuapp.com/',
+  baseURL: 'https://api-mylms.herokuapp.com/',
 });
 
 module.exports = {
